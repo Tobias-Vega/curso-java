6bc462d34aac1132b18eaa6196df4a78ff635473
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-16 15:47:05 -0300

    Bucles for

---
+++ 02-variables-primitivas/src/bucles/Bucles.java
@@ -0,0 +1,13 @@
+package bucles;
+
+public class Bucles {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Valor: " + i);
+        }
+
+        System.out.println("sale");
+
+    }
+}

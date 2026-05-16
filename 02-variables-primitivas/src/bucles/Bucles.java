37b6abb4e37374e24015b539df7f85b7d76970b1
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-16 15:56:26 -0300

    Bucles while y do while

--- 02-variables-primitivas/src/bucles/Bucles.java
+++ 02-variables-primitivas/src/bucles/Bucles.java
@@ -9,5 +9,23 @@ public class Bucles {
 
         System.out.println("sale");
 
+        int counter = 0;
+        int acumulador = 0;
+
+        while (counter < 5) {
+            System.out.println("Counter " + counter);
+            counter++;
+
+            acumulador = acumulador + counter;
+        }
+
+        System.out.println("Acumulador: " + acumulador);
+
+        int counter2 = 0;
+        do {
+            System.out.println("Counter 2: " + counter2);
+            counter2++;
+        } while (counter2 < 5);
+
     }
 }

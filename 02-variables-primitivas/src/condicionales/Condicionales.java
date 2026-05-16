044256deafb41280089a1e45a5d0e4f185ba60d5
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-16 13:47:45 -0300

    Condicionales - if y switch

---
+++ 02-variables-primitivas/src/condicionales/Condicionales.java
@@ -0,0 +1,31 @@
+package condicionales;
+
+public class Condicionales {
+    public static void main(String[] args) {
+        int age = 10;
+
+        if (age > 18) {
+            System.out.println("Es mayor a 18");
+        } else if (age < 0) {
+            System.out.println("No es válido");
+        } else {
+            System.out.println("Es menor a 18");
+        }
+
+        int day = 3;
+
+        switch (day) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            default:
+                System.out.println("El dato ingresado no es válido");
+        }
+    }
+}

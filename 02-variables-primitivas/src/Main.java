203265042c1a5eaaa19d2a0e0122385859d48511
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-16 13:15:01 -0300

    Variables primitivas

---
+++ 02-variables-primitivas/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        byte age = 23;
+        System.out.println("age = " + age);
+        float price = 19.99f;
+        double price2 = 19.99;
+        int xAxis, yAxis;
+        xAxis = 10;
+        yAxis = 20;
+
+        age = 30;
+        System.out.println("age = " + age);
+
+        System.out.println("xAxis = " + xAxis);
+        System.out.println("yAxis = " + yAxis);
+
+        char initial = 'G';
+        System.out.println("initial = " + initial);
+        boolean isTeacher = true;
+
+    }
+}

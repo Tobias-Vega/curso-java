0a2cde686786680bb51e0e2881387dab2d4dbde9
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-17 14:57:59 -0300

    Wrappers

---
+++ 03-clases-objetos/src/HouseExample/ClassWrapper.java
@@ -0,0 +1,10 @@
+package HouseExample;
+
+public class ClassWrapper {
+    public static void main(String[] args) {
+        int number = 10;
+        Integer number2 = Integer.valueOf(30);
+
+        System.out.println(number2);
+    }
+}

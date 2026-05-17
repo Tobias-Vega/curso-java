bca208521fa836d487c15a4d2234c662c08c1140
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-17 13:35:46 -0300

    Clases y objetos - Inicio sección 3

---
+++ 03-clases-objetos/src/HouseExample/House.java
@@ -0,0 +1,17 @@
+package HouseExample;
+
+public class House {
+    int doors;
+    int windows;
+    double size;
+
+    void openDoor() {
+        System.out.println("La puerta está abierta");
+    }
+
+    double calculateArea() {
+        return size * size;
+    }
+}
+
+

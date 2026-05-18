c8707f1e4a9da995f8a81c5dca0168547eb8ca89
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-18 00:00:21 -0300

    Método main() - Punto de entrada

---
+++ 03-clases-objetos/src/MainApp.java
@@ -0,0 +1,10 @@
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+        MainApp.sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hola, soy la aplicación principal");
+    }
+}

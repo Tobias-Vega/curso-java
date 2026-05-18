b11b9a278cd8c01d6ba3e3845acd69a6d4ae3417
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-17 22:35:14 -0300

    Métodos de clase - static

---
+++ 03-clases-objetos/src/metodoStatic/MainMath.java
@@ -0,0 +1,14 @@
+package metodoStatic;
+
+public class MainMath {
+    public static void main(String[] args) {
+        int result = MathExample.add(2, 2);
+        System.out.println("La suma es: " + result);
+
+        result = MathExample.subtract(10, 4);
+        System.out.println("La resta es: " + result);
+
+        result = MathExample.subtract(20, 4);
+        System.out.println("La resta es: " + result);
+    }
+}

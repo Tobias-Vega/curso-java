94257df7c07f7ff58251265c94e235138756d380
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-18 13:22:58 -0300

    Introducción a las estructuras de datos - Inicio sección 5

---
+++ 06-arrays-y-colecciones/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Integer[] numbers = {1, 2, 3};
+
+        System.out.println(numbers[0]);
+
+        numbers[0] = 100;
+
+        System.out.println(numbers[0]);
+
+        int[] fixedArray = new int[100];
+
+        System.out.println("Dimensión: " + fixedArray.length);
+
+        ArrayList<Integer> numbers2 = new ArrayList<>();
+
+        numbers2.add(1);
+        numbers2.add(10);
+        numbers2.add(100);
+
+        System.out.println(numbers2);
+
+    }
+}

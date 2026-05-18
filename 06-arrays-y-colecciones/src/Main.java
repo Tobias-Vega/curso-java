b3833559bfd9da20dbeae94c629d20206d2f84a4
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-18 15:18:45 -0300

    Arrays parte 2

--- 06-arrays-y-colecciones/src/Main.java
+++ 06-arrays-y-colecciones/src/Main.java
@@ -1,29 +1,43 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbersPrimitive = new int[5];
+        int[] numbersPrimitive = {1, 2, 3, 4, 5, 6, 0 ,9};
 
-        numbersPrimitive[0] = 10;
-        numbersPrimitive[1] = 20;
+        int deletePosition = 2;
 
-        int[] newNumbers = new int[numbersPrimitive.length + 1];
-        System.arraycopy(numbersPrimitive, 0, newNumbers, 0, numbersPrimitive.length);
-        newNumbers[5] = 500;
-
-        Integer[] numbersWrapper = new Integer[5];
-        numbersWrapper[0] = 30;
-        numbersWrapper[1] = null;
-
-        System.out.println("Tipo int");
-        for (int number: newNumbers) {
-            System.out.println(number);
+        for (int i = deletePosition; i < numbersPrimitive.length - 1; i++) {
+            numbersPrimitive[i] = numbersPrimitive[i + 1];
         }
 
-        System.out.println("Tipo Integer");
-        for (Integer number : numbersWrapper) {
+        numbersPrimitive[numbersPrimitive.length - 1] = 0;
+
+        numbersPrimitive[0] = 200;
+
+        for (int number: numbersPrimitive) {
             System.out.println(number);
+        }
+//        int[] numbersPrimitive = new int[5];
+//
+//        numbersPrimitive[0] = 10;
+//        numbersPrimitive[1] = 20;
+//
+//        int[] newNumbers = new int[numbersPrimitive.length + 1];
+//        System.arraycopy(numbersPrimitive, 0, newNumbers, 0, numbersPrimitive.length);
+//        newNumbers[5] = 500;
+//
+//        Integer[] numbersWrapper = new Integer[5];
+//        numbersWrapper[0] = 30;
+//        numbersWrapper[1] = null;
+//
+//        System.out.println("Tipo int");
+//        for (int number: newNumbers) {
+//            System.out.println(number);
+//        }
+//
+//        System.out.println("Tipo Integer");
+//        for (Integer number : numbersWrapper) {
+//            System.out.println(number);
+
+
         }
     }
-}

12ca348580c933c031fd2a6b5007a13a95514c38
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-19 11:23:33 -0300

    Entendiendo el método equals()

---
+++ 06-arrays-y-colecciones/src/com/devtalles/estructurasdedatos/list/equals/ExampleEquals.java
@@ -0,0 +1,11 @@
+package com.devtalles.estructurasdedatos.list.equals;
+
+public class ExampleEquals {
+    public static void main(String[] args) {
+        Integer numberA = 128; // 100
+        Integer numberB = 128; // 100 da true
+
+        System.out.println(numberB == numberA);
+        System.out.println(numberB.equals(numberA));
+    }
+}

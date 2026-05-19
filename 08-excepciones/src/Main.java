e010db1e38a28f0aa74386d1f5160b3a54404881
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-19 19:32:26 -0300

    Concepto básico de excepciones - Inicio sección 6

---
+++ 08-excepciones/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void recursive() {
+        recursive();
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            int result = 10 / 0;
+            System.out.println("Resultado: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("División por cero");
+        } finally {
+            System.out.println("Este mensaje se ejecuta siempre");
+        }
+
+        System.out.println("El programa finaliza");
+
+    }
+}

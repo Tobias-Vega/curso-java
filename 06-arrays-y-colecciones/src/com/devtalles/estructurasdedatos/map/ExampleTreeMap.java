a083b0e862ea8b1b72623f0f7b5f784e64315e21
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-19 13:17:00 -0300

    Map con wrapper: HashMap y TreeMap

---
+++ 06-arrays-y-colecciones/src/com/devtalles/estructurasdedatos/map/ExampleTreeMap.java
@@ -0,0 +1,30 @@
+package com.devtalles.estructurasdedatos.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ExampleTreeMap {
+    public static void main(String[] args) {
+        Map<String, Integer> prouductPrices = new TreeMap<>();
+
+        prouductPrices.put("Laptop", 1200);
+        prouductPrices.put("Smartphone", 1000);
+        prouductPrices.put("Tablet", 1500);
+        prouductPrices.put("Mouse", 1500);
+        prouductPrices.put("Keyboard", 1500);
+
+        System.out.println(prouductPrices);
+
+        System.out.println(prouductPrices.get("Laptop"));
+
+        prouductPrices.put("Tablet", 1234);
+
+        System.out.println(prouductPrices);
+
+        prouductPrices.remove("Laptop");
+
+        System.out.println(prouductPrices);
+
+    }
+}

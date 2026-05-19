fbc979a5fa8c9d0f509a5808ebee2c49aa92572e
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-19 20:04:03 -0300

    Refactorizando ejemplo

--- 08-excepciones/src/FileRead.java
+++ 08-excepciones/src/FileRead.java
@@ -1,28 +1,31 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileRead {
     public static void main(String[] args) {
 
+        BufferedReader reader = null;
+
         try {
-            FileReader file = new FileReader("src/example.txt");
-            BufferedReader reader = new BufferedReader(file);
+            reader = new BufferedReader(new FileReader("src/example.txt"));
+            String line;
 
-            String line = reader.readLine();
-
-            while (line != null) {
+            while ((line = reader.readLine()) != null) {
                 System.out.println(line);
-                line = reader.readLine();
             }
 
-            reader.close();
-
-        } catch (FileNotFoundException e) {
-            System.out.println("El archivo no existe");
         } catch (IOException e) {
             System.out.println("Error en la lectura del archivo");
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                    System.out.println("El archivo fue cerrado correctamente");
+                }
+            } catch (IOException e) {
+                System.out.println("Error al cerrar el archivo");
+            }
         }
 
     }
